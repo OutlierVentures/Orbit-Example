40cd1248b79fee46eddcbc254ee4ab43be64172e
Theo Turner <theo@Theos-MacBook-Pro.local>
2018-09-18 13:05:09 +0100

    Log db address

--- doc_db.js
+++ doc_db.js
@@ -7,16 +7,23 @@ const OrbitDB = require('orbit-db')
 
 // Pubsub option must be passed to all components
 const ipfsOptions = {
-  EXPERIMENTAL: {
-    pubsub: true
-  }
+    EXPERIMENTAL: {
+        pubsub: true
+    }
 }
 
-// Initialise OrbitDB instance on IPFS instance and create our
-// documents type database. We use the index-by-name option.
-// Don't forget we must pass the pubsub option in.
+// Create IPFS instance and pass the pubsub option in.
 const ipfs = new IPFS(ipfsOptions)
+
 ipfs.on('ready', () => {
-  const orbitdb = new OrbitDB(ipfs)
-  const db = await orbitdb.docs('docdb', { indexBy: 'name' })
+
+    // Create OrbitDB instance on the IPFS instance
+    const orbitdb = new OrbitDB(ipfs)
+
+    // Create our docs type database using index-by-name option
+    const db = await orbitdb.docs('docdb', { indexBy: 'name' })
+
+    // Log the database address so it can be replicated
+    console.log(db.address.toString())
+
 })
