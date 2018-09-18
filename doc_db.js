4f73267be1988cdf41e04a186b514e4dd122835e
Theo Turner <theo@Theos-MacBook-Pro.local>
2018-09-18 15:21:05 +0100

    Filtering + actual storage added to git

--- doc_db.js
+++ doc_db.js
@@ -15,15 +15,28 @@ const ipfsOptions = {
 // Create IPFS instance and pass the pubsub option in.
 const ipfs = new IPFS(ipfsOptions)
 
-ipfs.on('ready', () => {
+ipfs.on('ready', async () => {
 
     // Create OrbitDB instance on the IPFS instance
     const orbitdb = new OrbitDB(ipfs)
 
     // Create our docs type database using index-by-name option
-    const db = await orbitdb.docs('docdb', { indexBy: 'name' })
+    const db = await orbitdb.docs('doc_db', { indexBy: 'name' })
 
     // Log the database address so it can be replicated
     console.log(db.address.toString())
+
+    // Add three documents, e.g. representing people
+    await db.put({ _id: 'ID1', name: 'Alice', age: 16 })
+    await db.put({ _id: 'ID2', name: 'Bob', age: 17 })
+    await db.put({ _id: 'ID3', name: 'Carol', age: 18 })
+
+
+    // Filter docs
+    const youngsters = db.query((doc) => doc.age < 18)
+
+    for (i = 0; i < youngsters.length; i++) {
+        console.log(youngsters[i].name + " is underage!")
+    }
 
 })
